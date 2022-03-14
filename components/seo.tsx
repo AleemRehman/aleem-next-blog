87d413bdd6619134c8c036ed738d608f8a9d3124
Aleem Ur-Rehman <contact@aleemrehman.co>
2022-03-14 10:55:37 +0000

    bring in tailwind and typescript

---
+++ components/seo.tsx
@@ -0,0 +1,30 @@
+import { DefaultSeo } from "next-seo"
+
+const config = {
+  title: "Aleem Rehman - Solutions Architect & Mentor",
+  description:
+    "I’m a Solutions Architect that loves building effective products and writing about all the associated technologies!",
+  openGraph: {
+    type: "website",
+    locale: "en_GB",
+    url: "https://aleemrehman.co",
+    site_name: "Aleem Rehman",
+    // images: [
+    //   {
+    //     url: "test",
+    //     alt: "Aleem Rehman",
+    //   },
+    // ],
+  },
+  twitter: {
+    handle: "@almrhmn",
+    site: "@almrhmn",
+    cardType: "summary_large_image",
+  },
+}
+
+const SEO = (): JSX.Element => {
+  return <DefaultSeo {...config} />
+}
+
+export default SEO
