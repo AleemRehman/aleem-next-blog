d1c81083d0cc6695c30b731fe19c9014e8303b3a
Aleem Ur-Rehman <contact@aleemrehman.co>
2022-03-16 23:46:34 +0000

    add logo needs some cleanup

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import Image from "next/image"
 import styles from "components/header/header.module.scss"
 
 const links = [
@@ -20,6 +21,19 @@ const Header = (): JSX.Element => {
         <nav className="nav-container py-2 md:py-4">
           <div className="container px-4 mx-auto md:flex md:items-center">
             <div className="flex justify-between items-center">
+              <div className="flex justify-start lg:w-0 lg:flex-1">
+                <span className="">
+                  <Image
+                    alt="Aleem Rehman"
+                    height={48}
+                    width={48}
+                    src="https://res.cloudinary.com/dqgb6xtqy/image/upload/v1647442504/logo_cb65kg.png"
+                    placeholder="blur"
+                    blurDataURL="https://res.cloudinary.com/dqgb6xtqy/image/upload/v1647442504/logo_cb65kg.png"
+                    className="rounded-full"
+                  />
+                </span>
+              </div>
               <div
                 className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0"
                 id="navbar-collapse"
--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "variables.scss";`,
   },
+  images: {
+    domains: [
+      "s3.us-west-2.amazonaws.com", // Images coming from Notion
+      "via.placeholder.com", // for articles that do not have a cover image
+      "images.unsplash.com", // For blog posts that use an external cover ima ge
+      "pbs.twimg.com", // Twitter Profile Picture
+      "res.cloudinary.com",
+    ],
+  },
 }
